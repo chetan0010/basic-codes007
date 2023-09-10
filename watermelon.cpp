69c73d8ff9ceccee0a4ba447d9c207b6697f1b62
Chetan Joshi <108889812+chetan0010@users.noreply.github.com>
2023-09-10 21:23:49 +0530

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int w;
+    cin>>w;
+    if(w<=2){
+        cout<<"NO";
+        return 0;
+    }
+    
+    if(w%2!=0)
+    cout<<"NO";
+    else
+    cout<<"YES";
+    return 0;
+}
