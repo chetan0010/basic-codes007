40c54171c11dd090db1fcaea76b2ef281df091b3
Chetan Joshi <108889812+chetan0010@users.noreply.github.com>
2023-09-11 19:10:28 +0530

    Create search2Dmatrix.cpp

---
+++ search2Dmatrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int l=0;int r=m*n-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            int midval=matrix[mid/m][mid%m];
+            if(midval==target)
+            return true;
+            else if(midval<target)
+            {
+                l=mid+1;
+            }
+            else
+            r=mid-1;
+
+        }
+        return false;
+        
+        
+    }
+};
