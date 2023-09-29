9b563d23f676f2eb1464a65b69eeeb8eb9101328
Chetan Joshi <108889812+chetan0010@users.noreply.github.com>
2023-09-29 18:06:33 +0530

    Create tram.cpp

---
+++ tram.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int a,b,c=0,m=0;
+    cin>>n;
+    while(n--){
+        cin>>a>>b;
+        c=c-a;c=c+b;
+        if(c>m)m=c;
+    }
+    cout<<m;
+    return 0;
+}
