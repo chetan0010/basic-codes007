8c12018ffb78e48e2bc028defbc2695ad4a2e2f9
Chetan Joshi <108889812+chetan0010@users.noreply.github.com>
2023-10-02 10:22:07 +0530

    Create LuckyDivision.cpp

---
+++ LuckyDivision.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[] = {4,7,44,47,74,77,444,447,477,777,774,744};
+        for(int i=0; i<13; i++){
+            if(n%arr[i]==0){
+                cout<<"YES";
+                return 0;
+            }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+    
+    
+}
